f938df118abfef1af2b04add749bf88e6f177c50
sa148 <GitHub>
2022-04-09 09:26:47 +0000

    userコントローラー作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id =current_usre.id
+    @book.save
+    redirect_to book_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion,)
+  end
+
+end
