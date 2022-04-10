6726c68a4bae1fff2e168546b183a7a04e920b41
sa148 <GitHub>
2022-04-10 13:29:41 +0000

    log in

--- db/migrate/20220408105410_create_books.rb
+++ db/migrate/20220408105410_create_books.rb
@@ -1,10 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      
+
       t.string :title
       t.string :body
       t.text :caption
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_04_08_105410) do
     t.string "title"
     t.string "body"
     t.text "caption"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -23,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_04_08_105410) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
