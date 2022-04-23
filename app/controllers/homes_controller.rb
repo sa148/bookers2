ef1d2063a630aa28b18ea436fbdec5323303b179
sa148 <GitHub>
2022-04-23 06:13:14 +0000

    table

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,9 @@ class HomesController < ApplicationController
   def top
   end
 
+  def home
+  end
+
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  root to: 'homes#top'
  devise_for :users
- get "/homes/about" => "homes#about", as: "about"
+ get "/home/about" => "homes#about", as: "about"
  resources :books, only: [:new, :create, :show, :edit, :destroy, :index, :update]
  resources :users, only: [:new, :show, :create, :edit, :destroy, :index, :update]
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
